ed1dfbcf0fde13baf0ca6bb367a9cd2385b42b8b
omralhaj036-collab <omralhaj036@gmail.com>
2026-06-09 07:09:31 +0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const btn = document.getElementById("startBtn");
+
+    if(btn){
+        btn.addEventListener("click", () => {
+            alert("Welcome to SADAM VOX");
+        });
+    }
+
+});
